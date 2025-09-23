c64e0e04f50d87a298b4c4af360c70a7e93b0d4d
Serge Harutyunyan <s.harutyunyan0919@gmail.com>
2025-09-23 13:42:33 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    $('.top__banner__carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        autoplay:true,
+        autoplayTimeout:6000,
+        smartSpeed:800,
+        items: 1
+    })
+    $('.products__carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        stagePadding: 2,
+        autoplay:true,
+        autoplayTimeout:6000,
+        smartSpeed:800,
+        responsive: {
+            0: {
+                items: 1.5
+            },
+            600: {
+                items: 3.5
+            },
+            1000: {
+                items: 4.7
+            }
+        }
+    })
+
+});
